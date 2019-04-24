39b9839074b63e9753dfe28f471e9b70b624b339
Peter Trofimov <ptrofimov7@gmail.com>
2019-04-24 23:04:28 +0300

    Updates

---
+++ precache-manifest.314864ceb3743ddd36c14e96b33e797b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5993089f32833002c891f6b77b67281f",
+    "url": "/domino-react-app/index.html"
+  },
+  {
+    "revision": "a5e55f955872ddefda95",
+    "url": "/domino-react-app/static/css/main.77200db7.chunk.css"
+  },
+  {
+    "revision": "774d5e3722e17f42d013",
+    "url": "/domino-react-app/static/js/2.cba05053.chunk.js"
+  },
+  {
+    "revision": "a5e55f955872ddefda95",
+    "url": "/domino-react-app/static/js/main.f70b50b4.chunk.js"
+  },
+  {
+    "revision": "219ca66634292381bc7d",
+    "url": "/domino-react-app/static/js/runtime~main.921d2914.js"
+  }
+]);
